82e8a50c75226313964e3455370d4c44112a9e59
brotherrabbit91 <passiondev91@gmail.com>
2024-08-10 03:08:46 +0800

    feat/change all author

--- commit_author.py
+++ commit_author.py
@@ -1,4 +1,4 @@
-import subprocess
+# import subprocess
 
 def change_author(old_author_name, new_author_name, new_author_email):
     # Run git filter-branch to rewrite the author information
@@ -11,9 +11,15 @@ def change_author(old_author_name, new_author_name, new_author_email):
     # Run git commit --amend to apply the changes
     subprocess.run(['git', 'commit', '--amend', '--no-edit', '--reset-author'], check=True)
 
-if __name__ == "__main__":
-    old_author_name = "Neeraj giri"
-    new_author_name = "skystarxtogether"
-    new_author_email = "skystarxtogether@gmail.com"
+def change_all_authors(new_author_name, new_author_email):
+    # Run git filter-branch to rewrite all author information
+    subprocess.run(['git', 'filter-branch', '-f', '--env-filter',
+                    f'export GIT_AUTHOR_NAME="{new_author_name}"\n'
+                    f'export GIT_AUTHOR_EMAIL="{new_author_email}"\n'], check=True)
 
-    change_author(old_author_name, new_author_name, new_author_email)
+if __name__ == "__main__":
+    old_author_name = ""
+    new_author_name = ""
+    new_author_email = ""
+#     change_author(old_author_name, new_author_name, new_author_email)
+    change_all_authors(new_author_name, new_author_email)
